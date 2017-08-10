e1444fc54d90adba2181c0e86d68a15ce513f4f6
Joseph Choi <chousemath@gmail.com>
2017-08-10 12:01:49 +0900

    just a few more notes

---
+++ README.md
@@ -0,0 +1,3 @@
+### Building a Clojure project
+
+`lein uberjar``
--- clojure-practice-1/src/clojure_practice_1/core.clj
+++ clojure-practice-1/src/clojure_practice_1/core.clj
@@ -6,4 +6,9 @@
   [& args]
   (println "Hello, World!")
   (println "This is an addition by Joseph")
+  (println (first ["This is the first element" "Second" "Third"]))
+  (println "The value below should be 6")
+  (println (+ 1 2 3))
+  (println "The value below should be 24")
+  (println (* 2 3 4))
 )
