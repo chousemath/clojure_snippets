1d205a2b37cce6c1a93d72ee464f0fde3d42cbfa
Joseph Choi <chousemath@gmail.com>
2017-08-10 11:55:59 +0900

    first commit

---
+++ clojure-practice-1/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-practice-1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-practice-1/src/clojure_practice_1/core.clj
@@ -0,0 +1,9 @@
+(ns clojure-practice-1.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+  (println "This is an addition by Joseph")
+)
